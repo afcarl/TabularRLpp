1be4356172d70ad34e3071bc65a1508c4c4c49e2
Jiaming Song <jiaming.tsong@gmail.com>
2017-04-12 15:37:52 -0700

    average results for 100

--- ubev_eb/ubev_eb.cpp
+++ ubev_eb/ubev_eb.cpp
@@ -1,33 +1,31 @@
 #include "../chain_mdp.h"
 #include "ubev_eb.h"
 
-const int s_params[] = {100};//{100, 200};
-const double delta_params[] = {0.1};
-const double beta_params[] = {0.05};
 
-
-int main() {
+int main(int argc, char **argv) {
     const int num_episodes = 1000000;
-    for (auto s: s_params) {
-        for (auto delta: delta_params) {
-            for (auto beta: beta_params) {
-                std::shared_ptr<ChainMDP> chain_mdp(new ChainMDP(s, true, 5.0));
-                std::vector<double> rewards;
-                UBEV_EB ubev(s, 2, s, delta, beta, chain_mdp);
-                ubev.train(num_episodes, rewards);
-                std::string filename = "ubev_eb_" + std::to_string(s) + "-" + std::to_string(delta) + "-" + std::to_string(beta) + ".txt";
-                printf("%s\n", filename.c_str());
-                FILE *f = fopen(filename.c_str(), "w");
-                fprintf(f, "%d\n", s);
-                fprintf(f, "%lf\n", delta);
-                fprintf(f, "%lf\n", beta);
-
-                for (int i = 0; i < num_episodes; i++) {
-                    fprintf(f, "%.2lf\n", rewards[i]);
-                }
-                fclose(f);
-            }
-        }
+    if (argc < 3) {
+        return -1;
     }
+    int s = atoi(argv[1]);
+    double beta = atof(argv[2]);
+    double delta = 0.1;
+    std::string filename =
+            "ubev_eb_" + std::to_string(s) + "-" + std::to_string(delta) + "-" + std::to_string(beta) +
+            ".txt";
+    FILE *f = fopen(filename.c_str(), "w");
+    for (int run = 0; run < 5; run++) {
+        std::shared_ptr<ChainMDP> chain_mdp(new ChainMDP(s, true, 5.0));
+        std::vector<double> rewards;
+        UBEV_EB ubev(s, 2, s, delta, beta, chain_mdp);
+        ubev.train(num_episodes, rewards);
+        printf("%s\n", filename.c_str());
+
+        for (int i = 0; i < num_episodes; i++) {
+            fprintf(f, "%.2lf ", rewards[i]);
+        }
+        fprintf(f, "\n");
+    }
+    fclose(f);
     return 0;
 }
--- visualize.py
+++ visualize.py
@@ -13,6 +13,16 @@ def get_rewards(filename, prefix=''):
     return rewards[3:]
 
 
+def read_all(filename, prefix=''):
+    f = open(prefix + filename, 'r')
+    rewards = []
+    for line in f.readlines():
+        l = [float(s) for s in line.split()]
+        rewards.append(l)
+    rewards = np.concatenate([rewards], axis=0)
+    return rewards
+
+
 def running_mean(x, N):
     cumsum = np.cumsum(np.insert(x, 0, 0))
     return (cumsum[N:] - cumsum[:-N]) / N
@@ -22,6 +32,22 @@ def draw(x_list, l_list, N=20000, logscale=True):
     for x, l in zip(x_list, l_list):
         x_ = running_mean(x, N)
         plt.plot(x_, label=l)
+    plt.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.)
+    plt.gca().set_xlim([1000, 1000000])
+    if logscale:
+        plt.gca().set_xscale('log')
+    else:
+        plt.gca().set_xscale('linear')
+    plt.show()
+
+
+def draw_mean(x_list, l_list, N=20000, logscale=True, optimal=None):
+    for x, l in zip(x_list, l_list):
+        xm = np.mean(x, axis=0)
+        xm_ = running_mean(xm, N)
+        plt.plot(xm_, label=l)
+    if optimal:
+        plt.plot(np.arange(0, 1000000), np.ones([1000000]) * optimal, label='optimal')
     plt.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.)
     plt.gca().set_xlim([1000, 1000000])
     if logscale:
